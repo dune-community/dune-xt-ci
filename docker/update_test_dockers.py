31b9bcf6cbe9dba71451c8198b6689683940e6c1
Tobias Leibner <tobias.leibner@googlemail.com>
2021-08-24 13:51:51 +0200

    Fix dune-gdt-super CI

--- docker/update_test_dockers.py
+++ docker/update_test_dockers.py
@@ -164,7 +164,7 @@ def _build_combination(tag_matrix, dockerdir, module, commit, refname):
         print(f'Repo is: {repo}')
 
         with Timer('docker build ', logger.info):
-            buildargs = {'COMMIT': commit, 'CC': cc, 'project_name': module, 'BASE': settings['base']}
+            buildargs = {'COMMIT': commit, 'CC': cc, 'project_name': module, 'BASE': settings['base'], 'PROJECT': PROJECT}
             img = _docker_build(
                 client,
                 logger,
